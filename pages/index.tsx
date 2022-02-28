1f827ba3a6c238c095e4113d52eab4b3e4193f6f
Jim <jimke2000@gmail.com>
2022-02-28 13:25:48 +1100

    feat(pages): index
    Signed-off-by: Jim <jimke2000@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="text-center p-6 rounded-xl border-gray-200 border shadow-inner">
+        <h1 className="text-3xl">Hol&apos;up</h1>
+        <p className="text-xl">We&apos;re not done yet!</p>
+        <p className="text-xl">Website under construction</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
