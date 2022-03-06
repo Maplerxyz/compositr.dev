b3548136320a144a5cd52ac6113662f81ad64af5
Jim <jimke2000@gmail.com>
2022-03-06 20:41:15 +1100

    feat(windi): make concatenated classes work
    Signed-off-by: Jim <jimke2000@gmail.com>

--- windi.config.js
+++ windi.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from "windicss/helpers";
 
+function range(size, startAt = 1, increment = 1) {
+  return Array.from(Array(size).keys()).map(
+    (i) => increment * (i + 1) + startAt
+  );
+}
+
 export default defineConfig({
   extract: {
     include: ["**/*.{jsx,tsx,css}"],
     exclude: ["node_modules", ".git", ".next"],
   },
+  safelist: [
+    ...["green", "sky"].map((colour) =>
+      range(8, 100, 100).map((i) => {
+        return `hover:bg-${colour}-${i}`;
+      })
+    ),
+  ],
 });
