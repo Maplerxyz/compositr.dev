d81620dc803e293d9f4527a4c43da65cd2411959
Jim <jimke2000@gmail.com>
2022-03-07 19:20:33 +1100

    refactor(pages): use centerbox component
    Signed-off-by: Jim <jimke2000@gmail.com>

--- pages/i/[resourceID].tsx
+++ pages/i/[resourceID].tsx
@@ -1,15 +1,14 @@
 import type { GetServerSidePropsContext, NextPage } from "next";
 import ImageBox from "../../components/img/display/Image";
+import CenterBox from "../../components/utility/container/centerbox";
 
 const Image: NextPage = (props: any) => {
   const { resourceID } = props;
 
   return (
-    <div className="flex h-screen justify-center items-center">
-      <div className="p-6 shadow shadow-xl rounded-xl justify-center text-center bg-hex-323232">
-        <ImageBox resourceID={resourceID as string} />
-      </div>
-    </div>
+    <CenterBox>
+      <ImageBox resourceID={resourceID as string} />
+    </CenterBox>
   );
 };
 
