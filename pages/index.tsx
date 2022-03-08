8003d0c76901046c45bf48c36ac0770e101625f1
Jim <jimke2000@gmail.com>
2022-03-08 21:47:08 +1100

    feat(pages): title
    Signed-off-by: Jim <jimke2000@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex h-screen justify-center items-center">
-      <div className="text-center p-6 rounded-xl border-gray-200 border shadow-inner">
-        <h1 className="text-3xl">Hol&apos;up</h1>
-        <p className="text-xl">We&apos;re not done yet!</p>
-        <p className="text-xl">Website under construction</p>
+    <>
+      <Head>
+        <title>Compositr</title>
+      </Head>
+      <div className="flex h-screen justify-center items-center">
+        <div className="text-center p-6 rounded-xl border-gray-200 border shadow-inner">
+          <h1 className="text-3xl">Hol&apos;up</h1>
+          <p className="text-xl">We&apos;re not done yet!</p>
+          <p className="text-xl">Website under construction</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
