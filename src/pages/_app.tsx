a76e37382aae362e5379aafc8f26b91f96ced69c
Jim (Compositr) <jimke2000@gmail.com>
2022-05-15 13:04:20 +1000

    feat(pages): fix import
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import CoreLayout from "@/common/layouts/core/CoreLayout";
+import CoreLayout from "@/layouts/core/CoreLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
