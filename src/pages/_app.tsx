a418daba60fc6def6955eeecc33e6f9af01036de
Jim (Compositr) <jimke2000@gmail.com>
2022-05-15 16:24:44 +1000

    fix(pages): _templates
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import CoreLayout from "@/layouts/core/CoreLayout";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <CoreLayout>
-      <Component {...pageProps} />
-    </CoreLayout>
+    <>
+      <Head>
+        <meta name="description" content="Compositr - Coder and crypto supporter" />
+        <title>Compositr</title>
+      </Head>
+      <CoreLayout>
+        <Component {...pageProps} />
+      </CoreLayout>
+    </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en-AU">
-      <Head></Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
