0f03ec2613bbaad62b54bc47ec0602cc494b4bca
Jim (Compositr) <jimke2000@gmail.com>
2022-05-20 21:25:46 +1000

    feat(pages): 404
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Page404() {
+  return (
+    <div className="prose prose-white">
+      <h1>🤔 Something isn&apos;t right...</h1>
+      <p><strong>404</strong>, page not found</p>
+    </div>
+  );
+}
