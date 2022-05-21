734f396b5a31c561f13fded5f7ef00280b9faab0
Jim (Compositr) <jimke2000@gmail.com>
2022-05-21 20:29:04 +1000

    feat(common): update colours and blur on nav
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

--- src/common/components/core/Nav/NavBar.tsx
+++ src/common/components/core/Nav/NavBar.tsx
@@ -2,7 +2,7 @@ import NavBarButton from "./NavBarButton";
 
 export default function NavBar() {
   return (
-    <nav className="fixed z-[999] w-full sm:w-[80%] lg:w-[50rem] md:rounded-lg bg-slate-600/30 sm:border sm:border-slate-700 bg-opacity-80 backdrop-blur-lg flex justify-center sm:justify-evenly sm:mt-4 py-4 sm:py-2 px-2">
+    <nav className="fixed z-[999] w-full sm:w-[80%] lg:w-[50rem] md:rounded-lg bg-[#0e1320]/50 sm:border sm:border-slate-700 bg-opacity-80 backdrop-blur-lg flex justify-center sm:justify-evenly sm:mt-4 py-4 sm:py-2 px-2">
       <div className="">
         <NavBarButton href="/">Home</NavBarButton>
         <NavBarButton href="/contact">Contact Me</NavBarButton>
