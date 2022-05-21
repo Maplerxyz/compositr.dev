91347ef82c5bc81188e9a074530fdf2e26c4eecc
Jim (Compositr) <jimke2000@gmail.com>
2022-05-21 20:27:33 +1000

    feat: colours!
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

--- src/common/components/bars/Tech/TechBar.tsx
+++ src/common/components/bars/Tech/TechBar.tsx
@@ -2,9 +2,11 @@ import type { IconType } from "react-icons";
 
 export default function TechBar({ icons }: Props) {
   return (
-    <div className="px-2 mx-1 my-2 flex-row flex-wrap flex w-full justify-center gap-2 gap-y-3 md:justify-evenly">
+    <div className="p-4 mx-1 my-2 flex-row flex-wrap flex w-full justify-center gap-2 gap-y-3 md:justify-evenly border border-compositr/60 rounded-xl shadow-lg shadow-compositr/50">
       {icons.map((icon: IconType, i) => (
-        <div key={i} className="inline-block">{icon({ className: "h-6 w-6" })}</div>
+        <div key={i} className="inline-block">
+          {icon({ className: "h-6 w-6" })}
+        </div>
       ))}
     </div>
   );
